4dc4b229410a5b761fe48ec6aa1d70f5669794ac
Lee Avital <leeavital@gmail.com>
2013-02-11 21:59:07 -0500

    Added NAME= parsing

--- client_handler.erl
+++ client_handler.erl
@@ -5,26 +5,41 @@
 
 % start a client hanlder
 % Client :  a client socket
-% PPOstalService : the process that will send messages
+% PPostalService : the process that will send messages
 %  to all the other connected clients
 start( Client, PPostalService ) ->
   io:format("starting a client_handler\n"),
   gen_tcp:send(Client, ok),
-  loop( Client, PPostalService ). 
+  User = #user{},
+  loop( Client, PPostalService, User ). 
   
 
 
 % loop while receiveing from the client
 % same arguments as above 
-loop(Client, PPostalService) -> 
+loop(Client, PPostalService, User) -> 
   io:format("looping inside client_handler`\n"),
   case gen_tcp:recv(Client, 0) of 
       
-      {ok, Message} ->
-         io:format("received a message: ~p\n", [Message]),
-         PPostalService ! {send_all, Message},
-         loop(Client, PPostalService);    
-      {error, Message} -> 
-         io:format("there was a connection error: ~p, we are disconnecting this client", [Message])
-  end. 
+        % if we get a Message, parse it
+        {ok, Message} ->
+            case binary_to_list(Message) of
+                
+                % if we get a "NAME=" string, set the new name
+                "NAME=" ++ Name ->
+                    UpdatedUser = User#user{name=Name},
+                    io:format("setting the name to ~w", [Name]),
+                    loop(Client, PPostalService, UpdatedUser);
+
+                % if we can't find anything to parse, treat 
+                % it as a new message
+                _ -> 
+                    PPostalService ! {send_all, Message},
+                    loop(Client, PPostalService, User)
+            end;
+
+        % if there is an error, log the message to stdio 
+        {error, Message} -> 
+            io:format("there was a connection error: ~p, we are disconnecting this client", [Message])
+    end. 
     
--- main.erl
+++ main.erl
@@ -4,5 +4,4 @@
 
 % starts the main chat loop
 main( _ ) ->
-    test:get_person(),
     chat_main:start().
