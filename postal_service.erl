99f599b02be708497eeb76675cf221e978f6c68b
Lee Avital <leeavital@gmail.com>
2013-02-12 23:11:45 -0500

    Added comments in postal_service

--- postal_service.erl
+++ postal_service.erl
@@ -13,26 +13,32 @@ start() -> loop([]).
 % server loop and the client_handlers.
 % Connected : a list of connected clients (a list of sockets)
 loop(Connected) ->
-   receive 
-      {add_client, Client} ->
-         io:format("starting a client handler in loop\n"),
-         spawn( client_handler, start, [Client, self()] ),
-         io:format("spawned\n"),
-         loop(Connected ++ [Client]);
-      
-      {send_all, User, Message} ->
-         io:format("sending ~p to everyone", [Message]),
-         send_all(User, Message, Connected),
-         loop(Connected);
-      _ ->
-         io:format("received an unknown\n")
-   end.
+    receive 
+        
+        % add a client to the list of clients
+        {add_client, Client} ->
+            io:format("starting a client handler in loop\n"),
+            spawn( client_handler, start, [Client, self()] ),
+            io:format("spawned\n"),
+            loop(Connected ++ [Client]); 
+        
+        % send Message to all the clients from User
+        {send_all, User, Message} when is_record(User, user) ->
+            io:format("sending ~p to everyone", [Message]),
+            send_all(User, Message, Connected),
+            loop(Connected);
+        
+        % if we can't match it, log it to the stdio 
+        _ ->
+            io:format("received an unknown Message\n")
+    end.
 
 
 
 % send a message to all of the connected clients
 send_all( _, _, []) -> ok;
 
+% User : (#user) the user that sent the Message
 % Message : (sring) the message to send
 send_all(User, Message, [H|T]) ->
    gen_tcp:send(H, User#user.name ++ ": " ++ Message),
